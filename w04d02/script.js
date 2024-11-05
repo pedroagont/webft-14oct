61b9282f9be361dd62508a77330cd04250b1a3e6
Pedro A. González <pgonzalez@post.com>
2024-11-05 13:20:55 -0600

    add client js example

---
+++ w04d02/script.js
@@ -0,0 +1,34 @@
+console.log(window);
+console.log(navigator);
+console.log(document);
+
+const ctaBtn = document.getElementById('cta-btn');
+const destinationsList = document.getElementById('destinations-list');
+
+ctaBtn.addEventListener('click', (event) => {
+  console.log('hello from button!');
+  console.log(event.target);
+
+  const destinations = [
+    'Cancun',
+    'Playa del Carmen',
+    'Tulum',
+    'Riviera Maya',
+    'Cozumel',
+  ];
+
+  for (const dest of destinations) {
+    const destHtml = `<li>${dest}</li>`;
+    destinationsList.innerHTML += destHtml;
+  }
+});
+
+// document.addEventListener('click', (event) => {
+//   console.log('hello from document click!');
+//   console.log(event);
+// });
+
+// window.addEventListener('resize', (event) => {
+//   console.log('resizing!');
+//   console.log(event);
+// });
